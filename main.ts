f63dca466990738540c7e21313cf0f99aa4b0955
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-10-30 13:29:09 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables 
+let distanceOfObject: number = 0
+
+// set up 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// forever loop
+while (true) {
+    // get distance 
+    distanceOfObject = sonar.ping(
+        DigitalPin.P8,
+        DigitalPin.P12,
+        PingUnit.Centimeters
+    )
+    basic.clearScreen()
+    basic.showNumber(distanceOfObject)
+    // move 
+if (distanceOfObject <= 10) {
+    basic.showIcon(IconNames.No)
+    robotbit.StpCarMove(-10, 48)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+} else {
+    basic.showIcon(IconNames.Yes)
+    robotbit.StpCarMove(10, 48)
+}
+    }
